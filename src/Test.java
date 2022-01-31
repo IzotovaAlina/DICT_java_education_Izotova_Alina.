0b623bd4635923d819170744daf2d0e23f260577
IzotovaAlina <a.o.izotova@student.khai.edu>
2022-01-31 15:20:16 +0200

    Test.java added to feature_branch with change on string #6

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,10 @@
 public class Test {
     public static void main(String[] arg){
         System.out.println("education");
+
+        //new string
+        System.out.println("DICT");
     }
-    //new string
+
+
 }
